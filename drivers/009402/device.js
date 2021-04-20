92c32bc3afe57fb134856e8cf7a124e902348b15
Marco Ruiter <mruiter@tuxweb.nl>
2021-04-20 21:38:45 +0200

    Change tamper alarm 2 notification

--- drivers/009402/device.js
+++ drivers/009402/device.js
@@ -7,7 +7,7 @@ class P009402 extends ZwaveDevice {
 	onMeshInit() {
 		this.registerCapability('onoff', 'SWITCH_BINARY');
 		this.registerCapability('alarm_smoke', 'ALARM');
-		this.registerCapability('alarm_tamper', 'ALARM');
+		this.registerCapability('alarm_tamper', 'NOTIFICATION');
 		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
 	}
