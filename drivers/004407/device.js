ebbe444acc9a2b5352874082bc1f12e615016cf2
Marco Ruiter <mruiter@tuxweb.nl>
2021-04-20 21:32:36 +0200

    .

--- drivers/004407/device.js
+++ drivers/004407/device.js
@@ -7,7 +7,7 @@ class P004407 extends ZwaveDevice {
 	onMeshInit() {
 		this.enableDebug();
 		this.printNode();
-		this.registerCapability('alarm_co2', 'NOTIFICATION');
+		this.registerCapability('alarm_co', 'NOTIFICATION');
 		this.registerCapability('alarm_tamper', 'NOTIFICATION');
 		this.registerCapability('measure_battery', 'BATTERY');
 		this.registerCapability('alarm_battery', 'BATTERY');
